581f7e5c032753f87b8f92a072d34ce9e4a4edc5
MrBarratt <108690165+MrBarratt@users.noreply.github.com>
2022-07-18 22:39:39 +0200

    Stopwatch works!

--- StopWatch/stopwatch.js
+++ StopWatch/stopwatch.js
@@ -10,7 +10,7 @@ let displaySeconds = 0;
 
 // Function to increment timer
 function displayTimer(){
-
+	console.log("displayTimer",seconds,minutes,hours);
     //Increments seconds to minutes etc
     seconds++;
     if(seconds == 60){
@@ -46,18 +46,33 @@ function displayTimer(){
 };
 
 // START BUTTON
-const starter = document.getElementById("start");
-var timeStart = setTimeout(displayTimer);
+const startButton = document.getElementById("start");
+
+//define a variable that will be the reference to your "timer" interval
+var timerIntvl;
 
 //Make start button start timer
-starter.addEventListener('click', ()=> {
-  timeStart;
-  window.setInterval(displayTimer, 1000);  
+startButton.addEventListener('click', ()=> {
+  console.log("start");
+  //start your interval every 1000ms and run the function called displayTimer
+  timerIntvl = setInterval(displayTimer, 1000);  
 });
 
 // STOP BUTTON
-const stopTimer = document.getElementById("stop");
+const stopButton = document.getElementById("stop");
 
-stopTimer.addEventListener('click', ()=> {
-  clearTimeout(timeStart);
+stopButton.addEventListener('click', ()=> {
+  console.log("stop");
+  //stop the interval with the built in javascript method "clearInterval"
+  clearInterval(timerIntvl);
+});
+
+// RESET BUTTON
+const resetButton = document.getElementById("reset");
+
+resetButton.addEventListener('click', ()=> {
+ console.log("reset");
+ //reset everything back to 0
+ [hours, minutes, seconds] = [0, 0, 0];
+ timer.innerHTML =  "00:00:00";
 });
